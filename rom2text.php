805be0a72b46098e766dd683087645121c2dd2e7
hasegawa-tomoki <github@speedstars.jp>
2022-07-23 06:02:06 +0900

    Add address

--- rom2text.php
+++ rom2text.php
@@ -5,6 +5,9 @@ printf("std::array<uint8_t, %d> virtual_memory{\n", 0x100 + (strlen($bytes) / 2)
 
 // 0x0000-0x00ff
 for ($a = 0x0000; $a < 0x0100; $a++){
+  if ($a % 16 == 0){
+    outAddr($a);
+  }
   print('0x00, ');
   if ($a % 16 == 15){
     echo PHP_EOL;
@@ -13,6 +16,9 @@ for ($a = 0x0000; $a < 0x0100; $a++){
 // 0x0100-
 $cursor = 0;
 while ($cursor < strlen($bytes)){
+  if ($cursor % 32 == 0){
+    outAddr(0x0100 + $cursor / 2);
+  }
   $byte = substr($bytes, $cursor, 2);
   printf('0x%s, ', $byte);
   $cursor += 2;
@@ -22,3 +28,7 @@ while ($cursor < strlen($bytes)){
 }
 
 print('};');
+
+function outAddr(int $addr){
+  printf("/* 0x%04x */ ", $addr);
+}
