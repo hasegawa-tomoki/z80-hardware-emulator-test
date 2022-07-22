e45fdb8c18589ff9eaae37cd6d292d592824c575
hasegawa-tomoki <github@speedstars.jp>
2022-07-23 04:47:19 +0900

    Add C++ array def.

--- rom2text.php
+++ rom2text.php
@@ -1,5 +1,16 @@
 <?php
 $bytes = bin2hex(file_get_contents('prelim.rom'));
+
+printf("std::array<uint8_t, %d> virtual_memory{\n", 0x100 + (strlen($bytes) / 2));
+
+// 0x0000-0x00ff
+for ($a = 0x0000; $a < 0x0100; $a++){
+  print('0x00, ');
+  if ($a % 16 == 15){
+    echo PHP_EOL;
+  }
+}
+// 0x0100-
 $cursor = 0;
 while ($cursor < strlen($bytes)){
   $byte = substr($bytes, $cursor, 2);
@@ -9,3 +20,5 @@ while ($cursor < strlen($bytes)){
     echo PHP_EOL;
   }
 }
+
+print('}');
